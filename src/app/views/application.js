c93efdd68090a884d2a5c64d247f1cb2fab515c5
Jamie Pilgrim <hello@jamiepilgrim.com>
2016-12-13 09:40:09 -0800

    application view basic setup, no events

--- src/app/views/application.js
+++ src/app/views/application.js
@@ -1,11 +1,25 @@
 import Backbone from 'backbone';
+import TripListView from 'app/views/trip_list_view';
+
 
 const ApplicationView = Backbone.View.extend({
   initialize: function() {
     this.render();
   },
 
+  events: {
+    // save new trips
+    //
+  },
+
   render: function() {
+    const tripListView = new TripListView({
+      //check that .tripList is right
+      model: this.model.tripList,
+      el: this.$('main')
+    })
+    tripListView.render();
+
     return this;
   }
 });
