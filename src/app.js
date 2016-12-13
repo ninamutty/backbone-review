31c6f0c0a31504e8b40c2d8211d5d05326452959
Jamie Pilgrim <hello@jamiepilgrim.com>
2016-12-13 10:09:30 -0800

    app.js set seed data for 5 trips

--- src/app.js
+++ src/app.js
@@ -1,7 +1,35 @@
 import Application from 'app/models/application';
 import AppicationView from 'app/views/application_view';
 
-var application = new Application();
+var application = new Application({
+  trips: [
+    {
+      name: "Trip One",
+      continent: "Europe",
+      cost: 1000
+    },
+    {
+      name: "Trip Two",
+      continent: "Asia",
+      cost: 1500
+    },
+    {
+      name: "Trip Three",
+      continent: "Asia",
+      cost: 3000
+    },
+    {
+      name: "Trip Four",
+      continent: "South America",
+      cost: 900
+    },
+    {
+      name: "Trip Five",
+      continent: "Africa",
+      cost: 2000
+    }
+  ]
+});
 
 var appView = new ApplicationView({
   el: '#application',
