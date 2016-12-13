c2e19bf36b7d47a09796cf73afe5f6c12ae9663d
Jamie Pilgrim <hello@jamiepilgrim.com>
2016-12-13 09:27:30 -0800

    application model set

--- src/app/models/application.js
+++ src/app/models/application.js
@@ -1,7 +1,14 @@
 import Backbone from 'backbone';
+import Trip from 'app/models/trip';
+import TripList from 'app/collections/trips_list';
 
 const Application = Backbone.Model.extend({
-
+  initialize: function(options) {
+    var trips = options.trips.map(function(attrs){
+      return new
+    });
+    this.tripsList = new TripList(trips);
+  }
 });
 
 export default Application;
